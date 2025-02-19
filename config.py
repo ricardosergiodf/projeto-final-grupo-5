08d48d6546fd5f2cd18cd8ef814260ec5b5da67e
Murilo Amaral Campos <muriloamaralcampos478@gmail.com>
2025-02-19 10:33:58 -0300

    Config.py com paths

--- config.py
+++ config.py
@@ -12,4 +12,11 @@ URL_JADLOG = 'https://www.jadlog.com.br/jadlog/simulacao'
 
 #Nome arquivos
 NOME_EXCEL_PROCESSAR = 'Planilha de Entrada Grupos.xlsx'
+
+#PATHS
+PATH_RPA = r"C:\RPA"
+PATH_FALHAS = f"{PATH_RPA}\Falhas"
+PATH_LOGS = f"{PATH_RPA}\Logs"
+PATH_PROCESSADOS = f"{PATH_RPA}\Processados"
+PATH_PROCESSAR = f"{PATH_RPA}\Processar"
 #criar planilha de saída com o nome “cnpjs_dd-mm-aaaa_hh-mm-ss”
