08e39eff409503c3c301a2985c4846395c725201
Murilo Amaral Campos <muriloamaralcampos478@gmail.com>
2025-02-19 10:23:28 -0300

    Arquivo config.py criado

---
+++ config.py
@@ -0,0 +1,15 @@
+'''
+
+'''
+
+#from src import criar-diretorios
+
+#URLS
+URL_BRASIL_API = 'https://brasilapi.com.br/api/cnpj/v1/'
+URL_RPA_CHALLENGE = 'https://www.rpachallenge.com/'
+URL_CORREIOS = 'https://www2.correios.com.br/sistemas/precosPrazos/'
+URL_JADLOG = 'https://www.jadlog.com.br/jadlog/simulacao'
+
+#Nome arquivos
+NOME_EXCEL_PROCESSAR = 'Planilha de Entrada Grupos.xlsx'
+#criar planilha de saída com o nome “cnpjs_dd-mm-aaaa_hh-mm-ss”
